a7b06ac1367088086112e5b42db6d8e93cb900b1
ainhoa05 <64462241+ainhoa05@users.noreply.github.com>
2021-06-13 14:23:56 +0200

    Update main.c
    Ajout:
    fonction estMaj ->  permet de savoir si le caractère est une majuscule ou une minuscule
    Fonction  indice() -> retourne l'indice du caractère
    On peut encore reduire la fonction indice en utilisant la fonction est maj
    
    Ajout fonction cesar
    On doit creer une nouvelle fonction qui permet de demander la clef ou on demande la clef dans le main

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 	cd projet_c_iriart_ainhoa_tommy
 	gcc main.c -o main
 	./main
+	Exemple test
+	Pour cesar :
+	
+	
 */
 
 //Ajout bibliothèque
@@ -13,9 +17,14 @@
 #include <stdlib.h>
 #include <ctype.h>
 //En-tête fonction et procédure
+//1
 int nonautoriser(wchar_t *message,wchar_t *alphabetmin,wchar_t *alphabetmaj,wchar_t *alpahbetaccent);
 int verifexistaccent(wchar_t *message,wchar_t *alpahbetaccent);
 void convertionMessage(wchar_t *message,wchar_t *message_sans_accent,wchar_t *alphabetmaj,wchar_t *alphabetmin);
+//2
+int estmaj(wchar_t *alphabetmaj,wchar_t lettre);
+int indice(wchar_t *alphabetmin,wchar_t *alphabetmaj,wchar_t lettre);
+void cesar(wchar_t *message_sans_accent,wchar_t *message_convertie, wchar_t *alphabetmin,wchar_t *alphabetmaj);
 int main(void){
 	
 	//Déclaration des variables
@@ -35,7 +44,6 @@ int main(void){
   	int choix,choix_fichier;
 
 	//La saisie du message par l'utilisateur
-	
 	wprintf(L"Saisir votre message : ");
 	fgetws(message,200,stdin);
 	wprintf(L"Votre message : %ls \n",message);
@@ -47,10 +55,12 @@ int main(void){
 	}
 	
 	//Convertion message sans accent
-	wprintf(L"REp reponse accentexistane = %ld\n",verifexistaccent(message,alpahbetaccent));
 	convertionMessage(message,message_sans_accent,alphabetmaj,alphabetmin);
 	wprintf(L"Votre message sans accent : %ls \n",message_sans_accent);
 	
+	wprintf(L"\n¤ Execution cesar ¤\n");
+	cesar(message_sans_accent,message_convertie, alphabetmin,alphabetmaj);
+	wprintf(L"\nVotre message convertie: %ls \n",message_convertie);
 }
 
 int nonautoriser(wchar_t *message,wchar_t *alphabetmin,wchar_t *alphabetmaj,wchar_t *alpahbetaccent){
@@ -62,7 +72,7 @@ int nonautoriser(wchar_t *message,wchar_t *alphabetmin,wchar_t *alphabetmaj,wcha
         while(j<26){
                
           if (message[i]==alphabetmin[j] || message[i]==alphabetmaj[j] || message[i]==' '){
-            contenir+=1;
+          	contenir+=1;
           }
           j++;
         }
@@ -103,76 +113,134 @@ void convertionMessage(wchar_t *message,wchar_t *message_sans_accent,wchar_t *al
 	int i=0;
 	while(message[i]!='\n'){
 		switch(message[i]){
-		//A majuscule position 0 dans l alphabet
-		case L'À': case L'Á': case L'Â': case L'Ã': case L'Ä': case L'Å':
-			message_sans_accent[i]=alphabetmaj[0];
-			break;
-		//A minuscule
-		case L'à': case L'á': case L'â': case L'ã': case L'ä': case L'å':
-			message_sans_accent[i]=alphabetmin[0];
-			break;
-		//C majuscule position 2 dans l alphabet
-		case L'Ç':
-			message_sans_accent[i]=alphabetmaj[2];
-			break;
-		//C minuscule
-		case L'ç':
-			message_sans_accent[i]=alphabetmin[2];
-			break;
-		// E majuscule position 4 dans l alphabet
-		case L'È': case L'É': case L'Ê': case L'Ë':
-			message_sans_accent[i]=alphabetmaj[4];
-			break;
-		//E minuscule
-		case L'è': case L'é': case L'ê': case L'ë':
-			message_sans_accent[i]=alphabetmin[4];
-			break;
-		// I majuscule position 8 dans l'alphabet
-		case L'Ì': case L'Í': case L'Î': case L'Ï':
-			message_sans_accent[i]=alphabetmaj[8];
-			break;
-		//I minuscule
-		case L'ì': case L'í': case L'î': case L'ï':
-			message_sans_accent[i]=alphabetmin[8];
-			break;
+			//A majuscule position 0 dans l alphabet
+			case L'À': case L'Á': case L'Â': case L'Ã': case L'Ä': case L'Å':
+				message_sans_accent[i]=alphabetmaj[0];
+				break;
+			//A minuscule
+			case L'à': case L'á': case L'â': case L'ã': case L'ä': case L'å':
+				message_sans_accent[i]=alphabetmin[0];
+				break;
+			//C majuscule position 2 dans l alphabet
+			case L'Ç':
+				message_sans_accent[i]=alphabetmaj[2];
+				break;
+			//C minuscule
+			case L'ç':
+				message_sans_accent[i]=alphabetmin[2];
+				break;
+			// E majuscule position 4 dans l alphabet
+			case L'È': case L'É': case L'Ê': case L'Ë':
+				message_sans_accent[i]=alphabetmaj[4];
+				break;
+			//E minuscule
+			case L'è': case L'é': case L'ê': case L'ë':
+				message_sans_accent[i]=alphabetmin[4];
+				break;
+			// I majuscule position 8 dans l'alphabet
+			case L'Ì': case L'Í': case L'Î': case L'Ï':
+				message_sans_accent[i]=alphabetmaj[8];
+				break;
+			//I minuscule
+			case L'ì': case L'í': case L'î': case L'ï':
+				message_sans_accent[i]=alphabetmin[8];
+				break;
+				
+			//N majuscule position 13 dans l'alphabet
+			case L'Ñ':
+				message_sans_accent[i]=alphabetmaj[13];
+				break;
+			//I minuscule
+			case L'ñ':
+				message_sans_accent[i]=alphabetmin[13];
+				break;
 			
-		//N majuscule position 13 dans l'alphabet
-		case L'Ñ':
-			message_sans_accent[i]=alphabetmaj[13];
-			break;
-		//I minuscule
-		case L'ñ':
-			message_sans_accent[i]=alphabetmin[13];
-			break;
-		
-		//o majuscule position 14 dans l'alphabet 
-		case L'Ò': case L'Ó': case L'Ô': case L'Õ': case L'Ö':
-			message_sans_accent[i]=alphabetmaj[14];
-			break;
-		//o minuscule 
-		case L'ò': case L'ó': case L'ô': case L'õ': case L'ö':
-			message_sans_accent[i]=alphabetmin[14];
-			break;
-		
-		
-		//u majuscule position 20 dans l'alphabet 
-		case L'Ù': case L'Ú': case L'Û': case L'Ü':
-			message_sans_accent[i]=alphabetmaj[20];
-			break;
-		//u minuscule 
-		case L'ù': case L'ú': case L'û': case L'ü':
-			message_sans_accent[i]=alphabetmin[20];
-			break;
-		//y majuscule positin 24
-		case L'Ý':
-			message_sans_accent[i]=alphabetmaj[24];
-			break;
-		//y minuscule 
-		case L'ý': case L'ÿ':
-			message_sans_accent[i]=alphabetmin[24];
-			break;
-		default:
-			message_sans_accent[i]=message[i];
+			//o majuscule position 14 dans l'alphabet 
+			case L'Ò': case L'Ó': case L'Ô': case L'Õ': case L'Ö':
+				message_sans_accent[i]=alphabetmaj[14];
+				break;
+			//o minuscule 
+			case L'ò': case L'ó': case L'ô': case L'õ': case L'ö':
+				message_sans_accent[i]=alphabetmin[14];
+				break;
+			
+			
+			//u majuscule position 20 dans l'alphabet 
+			case L'Ù': case L'Ú': case L'Û': case L'Ü':
+				message_sans_accent[i]=alphabetmaj[20];
+				break;
+			//u minuscule 
+			case L'ù': case L'ú': case L'û': case L'ü':
+				message_sans_accent[i]=alphabetmin[20];
+				break;
+			//y majuscule positin 24
+			case L'Ý':
+				message_sans_accent[i]=alphabetmaj[24];
+				break;
+			//y minuscule 
+			case L'ý': case L'ÿ':
+				message_sans_accent[i]=alphabetmin[24];
+				break;
+			default:
+				message_sans_accent[i]=message[i];
+			}
+		i++;
+	}
+}
+//fonction qui regarde s il s agit d'une maj ou d une min maj=0 et min=1
+int estmaj(wchar_t *alphabetmaj,wchar_t lettre){
+	//Déclaration des variables
+	int i=0;
+	//debut boucle
+	while(i<26){
+		if(lettre==alphabetmaj[i]){
+			return 0;
+		}
+   	 	i++;
+  	}
+  	return 1;
+}
+// fonction qui retourne indice en fonction de l'alphabet
+int indice(wchar_t *alphabetmin,wchar_t *alphabetmaj,wchar_t lettre){
+	//Déclaration des variables
+	int i=0;
+	
+	//debut boucle
+	while(i<26){
+		if(lettre==alphabetmin[i]){
+			return i;
+		}else if(lettre==alphabetmaj[i]){
+			return i;
+		}
+		i++;
+	}
+}
+
+void cesar(wchar_t *message_sans_accent,wchar_t *message_convertie, wchar_t *alphabetmin,wchar_t *alphabetmaj){
+	//Déclaration des variables
+	int clef,i,indice_lettre_message,somme;
+	i=0;
+	
+	//demande clef
+	wprintf(L"\nSaisir la clef : ");
+	wscanf(L"%ld",&clef);
+	wprintf(L"Clé saisi : %ld ",clef);
+
+  	//debut boucle
+	while(i<wcslen(message_sans_accent)){
+		if(message_sans_accent[i]==' '){
+			message_convertie[i]=' ';
+		}else{
+			indice_lettre_message=indice(alphabetmin,alphabetmaj,message_sans_accent[i]);
+			somme=indice_lettre_message+clef;
+			if(somme>=26){
+				somme%=26;
+			}
+			if(estmaj(alphabetmaj,message_sans_accent[i])==0){
+				message_convertie[i]=alphabetmaj[somme];
+			}else{
+				message_convertie[i]=alphabetmin[somme];
+			}
 		}
 		i++;
 	}
