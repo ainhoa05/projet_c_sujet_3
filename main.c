e2f14f7845ff143d1e8ed6df80c281f94103739f
ainhoa05 <64462241+ainhoa05@users.noreply.github.com>
2021-06-13 23:41:22 +0200

    Update main.c
    Refait tous le code pour corriger le chiffre negati
    Ajout:
    fonction est Maj
    Fonction indice
    Fonction cesar
    
    Ajout d'un menu pour choisir l algo
    probleme avec le scanF

--- main.c
+++ main.c
@@ -5,6 +5,13 @@
 	./main
 	Exemple test
 	Pour cesar :
+	dCode César
+	cBncd Bdrzq
+	clef: -1
+	
+	Caé oui
+	Ayc msg
+	clef:24
 	
 	
 */
@@ -21,6 +28,9 @@
 int nonautoriser(wchar_t *message,wchar_t *min,wchar_t *maj,wchar_t *accent);
 int verifexistaccent(wchar_t *message,wchar_t *accent);
 void convertionMessage(wchar_t *message,wchar_t *messageSansAccent,wchar_t *maj,wchar_t *min);
+int estmaj(wchar_t *maj,wchar_t lettre);
+int indice(wchar_t *min,wchar_t *maj,wchar_t lettre);
+void cesar(wchar_t *messageSansAccent,wchar_t *messageConvertie, wchar_t *min,wchar_t *maj,int clefC);
 int main(void){
 	struct lconv *loc;
 	setlocale (LC_ALL, "");
@@ -33,10 +43,12 @@ int main(void){
 	wchar_t maj[]=L"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   	wchar_t accent[]=L"ÀÁÂÃÄÅàáâãäåÇçÈÉÊËèéêëÌÍÎÏìíîïÑñÒÓÔÕÖòóôõöÙÚÛÜùúûüÝýÿ";
 	
+	wchar_t messageConvertie[200]={0};
+	int choixAlgo;
+	int clefC;
 	// Demande de saisir le message
 	wprintf(L"\n - Saisir votre message : ");
 	fgetws(message,200,stdin);
-	wprintf(L"   Votre message : %ls \n",message); //verif
 	
 	// Vérification caractère non autoriser
 	while(nonautoriser(message,min,maj,accent)==2){
@@ -45,19 +57,57 @@ int main(void){
 		fgetws(message,200,stdin);
 	}
 	
-	wprintf(L"   Votre message : %ls \n",message); //verif
-	
 	// Convertion message
 	if(verifexistaccent(message,accent)==1){
-		wprintf(L"  -Votre message contient des accents. \n");
+		wprintf(L" - Votre message contient des accents. \n");
 		wprintf(L"   Convertion en cour ... \n");
 	}else{
-		wprintf(L"  -Votre message ne contient pas d'accent. \n");
+		wprintf(L" - Votre message ne contient pas d'accent. \n");
 	}
 	convertionMessage(message,messageSansAccent,maj,min);
 	wprintf(L"   Votre message : %ls \n",messageSansAccent);
+	
+	// Choix de l'algorithme
+	wprintf(L" - Veuillez choisir un algorithme :  \n");
+	wprintf(L"  	1) Chiffrement Cesar  \n");
+	wprintf(L"  	2) Chiffrement Vigenere\n");
+	wprintf(L"  	3) Dechiffrement Vigenere\n");
+	wprintf(L"\n   Saisir votre algorithme : ");
+	wscanf(L"%d",&choixAlgo);
+	wprintf(L"   Votre choix : %d \n",choixAlgo);
+	
+	// Verification 
+	while(choixAlgo<1 || choixAlgo>3){
+		wprintf(L"\n -! ERREUR : Votre choix doit etre 1, 2 ou 3 !-\n");
+		wprintf(L"    Resaisir votre choix : ");
+		wscanf(L"%d",&choixAlgo);
+	}
+	//selection de l'algoritme
+	switch(choixAlgo){
+		case 1:
+			wprintf(L"\n ------   Chiffrement Cesar   ------\n\n");
+			wprintf(L"\n - Saisir votre clef : ");
+			wscanf(L"%d",&clefC);
+			wprintf(L"   Votre Clef : %d \sn",clefC);
+			cesar(messageSansAccent,messageConvertie,min,maj,clefC);
+			wprintf(L"   Votre message convertie: %ls \n",messageConvertie);
+			break;
+		case 2:
+			wprintf(L"\n ------   Chiffrement Vigenere   ------\n\n");
+			//wprintf(L"\n - Saisir votre clef : ");
+			//wscanf(L"%d",&clefC);
+			//wprintf(L"   Votre Clef : %d \n",clefC);
+			//break;
+		case 3:
+			wprintf(L"\n ------   Dechiffrement Vigenere   ------\n\n");
+			//wprintf(L"\n - Saisir votre clef : ");
+			//wscanf(L"%d",&clefC);
+			//wprintf(L"   Votre Clef : %d \n",clefC);
+			//break;
+		
+	}
 }
-
+s
 int nonautoriser(wchar_t *message,wchar_t *min,wchar_t *maj,wchar_t *accent){
 	// Déclaration des variables
 	int contenir=0,i=0,j,v;
@@ -107,7 +157,8 @@ int verifexistaccent(wchar_t *message,wchar_t *accent){
 void convertionMessage(wchar_t *message,wchar_t *messageSansAccent,wchar_t *maj,wchar_t *min){
 	// Déclaration des variables
 	int i=0;
-	// Boucle de convertion
+	
+	// Boucle de convertions
 	while(message[i]!='\n'){
 		switch(message[i]){
 		//A majuscule position 0 dans l alphabet
@@ -126,7 +177,7 @@ void convertionMessage(wchar_t *message,wchar_t *messageSansAccent,wchar_t *maj,
 		case L'ç':
 			messageSansAccent[i]=min[2];
 			break;
-		// E majuscule position 4 dans l alphabet
+		// E majuscule position 4 dans l alphasbet
 		case L'È': case L'É': case L'Ê': case L'Ë':
 			messageSansAccent[i]=maj[4];
 			break;
@@ -179,4 +230,66 @@ void convertionMessage(wchar_t *message,wchar_t *messageSansAccent,wchar_t *maj,
 		}
 		i++;
 	}
+}
+int estmaj(wchar_t *maj,wchar_t lettre){
+	// Déclaration des variables
+	int i=0;
+  
+  	//Boucle qui verifie si la lettre est une majuscule 
+	while(i<26){
+		if(lettre==maj[i]){
+			return 0;
+		}
+		i++;
+	}
+	return 1;
+}
+int indice(wchar_t *min,wchar_t *maj,wchar_t lettre){
+	// Déclaration des variables
+	int i=0;
+	
+	//Boucle qui retourne l'indice de la lettre si elle est dans l'alphabet des minuscules ou des majuscules  
+	while(i<26){
+		if(lettre==min[i]){
+			return i;
+		}
+		i++;
+	}
+	i=0;
+	while(i<26){
+		if(lettre==maj[i]){
+			return i;
+		}
+		i++;
+	}
+}
+
+void cesar(wchar_t *messageSansAccent,wchar_t *messageConvertie, wchar_t *min,wchar_t *maj,int clefC){
+	// Déclaration des variables
+	int i,majOuMin,indiceLMessage,somme;
+
+  	//debut programme
+  	//Si clef est négatif on ajouter 26
+  	while (clefC<0){
+  		clefC+=26;
+  	}
+	i=0;
+	while(i<wcslen(messageSansAccent)){
+		if(messageSansAccent[i]==' '){
+			messageConvertie[i]=' ';
+		}else{
+			majOuMin=estmaj(maj,messageSansAccent[i]);
+			indiceLMessage=indice(min,maj,messageSansAccent[i]);
+			somme=indiceLMessage+clefC;
+			if(somme>=26){
+				somme%=26;
+			}
+			if(majOuMin==0){
+				messageConvertie[i]=maj[somme];
+			}else{
+				messageConvertie[i]=min[somme];
+	      		}
+		}
+		i++;
+  	}
 }
